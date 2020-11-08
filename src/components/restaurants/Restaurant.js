d08705b832a19badc76b6db592e2960cb955cbee
StevenDRiggs <sfriggs21@gmail.com>
2020-11-08 00:29:01 -0700

    began fleshing out restaurants; pre- "retry-branch" commit

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
+
 import ReviewsContainer from '../../containers/ReviewsContainer';
 
+
 class Restaurant extends Component {
-
-
   render() {
     const { restaurant } = this.props;
 
     return (
       <div>
-        <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
-        </li>
+        {restaurant.text}
+        <button> X </button>
+        <ReviewsContainer restaurant={restaurant}/>
       </div>
     );
   }
 };
+
 
 export default Restaurant;
--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 
+
+const initialState ={
+  text: '',
+}
+
+
 class RestaurantInput extends Component {
+  state = {
+    ...initialState,
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state)
+    this.setState({
+      ...initialState,
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            name='text'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
 };
+
 
 export default RestaurantInput;
