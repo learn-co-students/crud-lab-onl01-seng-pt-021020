825cd725ee20a7c13e32c3855689f1e1b69707fa
jkatem <jenmiao@gmail.com>
2020-12-01 16:50:03 -0800

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    });
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
 export default RestaurantInput;
+
+// This component is to allow users to create a review on a specific restuarant.  
+// need to create a form to handle inputs
+// need to establish initial state
+// need to create handleOnChange function and handleOnSubmit function 
+// inside render return, is where I call my handle functions.
+// I need to make a connection
